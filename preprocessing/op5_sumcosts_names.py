c31ac220e55420dcc72b1acb0ad3f3c3a4f6ac54
Gilda Savaia <gilda.savaia@Alcampus.it>
2026-03-29 16:35:28 +0200

    bug fix

--- preprocessing/op5_sumcosts_names.py
+++ preprocessing/op5_sumcosts_names.py
@@ -9,7 +9,6 @@ class op5_sumcosts_namesResult:
 def run_op5(df: pd.DataFrame) -> op5_sumcosts_namesResult:
     """
     OP5 completa:
-    - Sostituisce NaN nei costi con 0
     - Crea feature TotalSpending
     - Rimuove Names e Surnames
     """
@@ -19,13 +18,11 @@ def run_op5(df: pd.DataFrame) -> op5_sumcosts_namesResult:
     # Colonne di costo
     cost_cols = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 
-    # 1. Pulizia NaN
-    df_processed[cost_cols] = df_processed[cost_cols].fillna(0)
 
-    # 2. Feature aggregata
+    # 1 Feature aggregata
     df_processed['TotalSpending'] = df_processed[cost_cols].sum(axis=1)
 
-    # 3. Rimozione colonne inutili
+    # 2. Rimozione colonne inutili
     colonne_da_rimuovere = ['Names', 'Surnames']
     colonne_esistenti = [col for col in colonne_da_rimuovere if col in df_processed.columns]
     df_processed = df_processed.drop(columns=colonne_esistenti)
